4996b324d4b46a81c122e3092deac458fb13deee
Tapabrata Gayen <tapabratagayen@gmail.com>
2025-06-21 01:41:03 +0530

    Modify body, add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-import "./App.css";
 import Navbar from "./Component/NavBar/Navbar";
 import HeroSection from "./Component/HeroSection/HeroSection"; 
-
-
 import Body from './Component/Body/Body.jsx';
 import About from './Component/About/About';
 import Footer from './Component/Footer/Footer';
@@ -13,16 +9,9 @@ export default function App() {
     <>
       <Navbar />
       <HeroSection />
-      
-    </>
-  );
-}
-
-export default App;
-    <div className="bg-[#F5F5F5DE] min-h-screen text-black">
       <Body />
       <About />
       <Footer />
-    </div>
+    </>
   );
 }
