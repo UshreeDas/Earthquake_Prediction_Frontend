5823294988d34bc7a13e74af97bc6a8252e46f2b
Tapabrata Gayen <tapabratagayen@gmail.com>
2025-06-20 20:54:12 +0530

    frontend body, about, footer

---
+++ src/Component/About/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className=" mx-6 my-6 p-6 rounded-xl text-center">
+      <h2 className="font-bold text-2xl mb-4">About Bhūkamp Sūchak</h2>
+      <p className="text-gray-700">
+        Our mission is to advance earthquake prediction science and provide accessible information 
+        to help communities across India prepare for seismic events.
+      </p>
+    </div>
+  );
+}
